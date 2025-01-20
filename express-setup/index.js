322ca2935822d4f43dbf95cff0e738f04b4ffd24
Chandra-Devloper <chandramathim5172@gmail.com>
2025-01-20 13:16:00 +0530

    post , get , delete - api

--- express-setup/index.js
+++ express-setup/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.use(express.raw());
+
 app.get('/',(req,res)=>{
+    res.statusCode = 200;
     res.send("Welcome Express")
+})
+
+app.post('/', (req, res)=>{
+    console.log("check post",req.body)
+    const {name} = req.body;
+    
+    res.send(`Welcome ${name}`);
+})
+
+app.delete('/', (req, res)=>{
+    const {name} = req.body;
+    console.log('req', req)
+    res.send(`Deleted user successfully ${name}`);
 })
 
 app.listen(4000 , ()=>{
